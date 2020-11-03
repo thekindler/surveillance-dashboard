5fcb42eb0e48b116258036ed541457bd724d42e0
anupam <anupamb266@gmail.com>
2020-11-03 15:28:08 +0530

    bkp commit

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ErrorComponent } from './layouts/error/error.component';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
@@ -31,7 +32,7 @@ import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
       disableConsoleLogging: false,
     }),
   ],
-  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent],
+  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent, DashboardComponent],
   bootstrap: [MainComponent],
 })
 export class SurveillanceAppModule {}
